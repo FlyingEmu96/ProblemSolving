06c1b502a8a7eec1a9949e81179d7919ed73eb9e
Emir_Flying_Emu96 <emir.murat1337@gmail.com>
2024-07-30 22:21:34 +0300

    0.1_ODD_EVEN.c

---
+++ Level_1_problems.c
@@ -0,0 +1,54 @@
+/*Level 1 Problems
+Odd or Even:
+Write a program that takes an integer as input and determines whether it is odd or even..
+Solution on lines 30+ give it a go first
+*/
+
+
+
+
+int main(){
+  
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*#include <stdio.h>
+
+int main() {  
+    int number;
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    if (number % 2 == 0)
+        printf("%d is even.\n", number);
+    else
+        printf("%d is odd.\n", number);
+
+    return 0;
+}*/
